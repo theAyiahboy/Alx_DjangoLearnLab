15ae5980e857f18cafc6e3ed1b5ea5e6db1eda81
Giovanni <giovanniayiahmensah@gmail.com>
2025-11-06 22:06:07 +0000

    fix

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,26 +1,3 @@
-"""
-URL configuration for django_models_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
 from django.urls import path
 from . import views
 from .views import LibraryDetailView
@@ -30,24 +7,11 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
 
-from django.urls import path, include
 
-urlpatterns = [
-    path('relationship_app/', include('relationship_app.urls')),
-]
-
-from django.urls import path
-from . import views
-from .views import LibraryDetailView
-
-urlpatterns = [
-    path('books/', views.list_books, name='list_books'),  # function-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # class-based view
-]
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('relationship_app/', include('relationship_app.urls')),  # 👈 this line is required
+    path('relationship_app/', include('relationship_app.urls')),  # 👈 important
 ]

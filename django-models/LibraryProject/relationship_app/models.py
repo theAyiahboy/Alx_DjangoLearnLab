c6b49cada1bfeec256d0e06632637e0ea808158a
Giovanni <giovanniayiahmensah@gmail.com>
2025-11-08 00:53:41 +0000

    trials

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Author Model
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Book Model
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+# Library Model
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+# Librarian Model
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name

01f51f8d1256c9d5fc8a3c8f93621cb625f8a151
Giovanni <giovanniayiahmensah@gmail.com>
2025-11-08 01:27:22 +0000

    fix it

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView  # Explicit imports
 
 urlpatterns = [
     # Function-based view
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
 
     # Class-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]

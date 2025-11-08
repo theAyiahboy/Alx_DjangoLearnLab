0594434aa61a27b931a8e7a021e8fd67081f5abe
Giovanni <giovanniayiahmensah@gmail.com>
2025-11-08 02:04:42 +0000

    fix it

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('admin-view/', admin_view, name='admin_view'),
-    path('librarian-view/', librarian_view, name='librarian_view'),
-    path('member-view/', member_view, name='member_view'),
-    path('book/add/', add_book, name='add_book'),
-    path('book/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('book/<int:pk>/delete/', delete_book, name='delete_book')
+    path("books/", list_books, name="list_books"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("admin-view/", admin_view, name="admin_view"),
+    path("librarian-view/", librarian_view, name="librarian_view"),
+    path("member-view/", member_view, name="member_view"),
+    path("book/add/", add_book, name="add_book"),
+    path("book/<int:pk>/edit/", edit_book, name="edit_book"),
+    path("book/<int:pk>/delete/", delete_book, name="delete_book")
 ]

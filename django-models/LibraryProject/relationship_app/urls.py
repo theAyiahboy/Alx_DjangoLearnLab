e0a6c11db38bf826505be9aaae0851f878a55dd1
Giovanni <giovanniayiahmensah@gmail.com>
2025-11-08 22:55:54 +0000

    correct urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
+from . import views
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("admin-view/", admin_view, name="admin_view"),
-    path("librarian-view/", librarian_view, name="librarian_view"),
-    path("member-view/", member_view, name="member_view"),
-    path("book/add/", add_book, name="add_book"),
-    path("book/<int:pk>/edit/", edit_book, name="edit_book"),
-    path("book/<int:pk>/delete/", delete_book, name="delete_book")
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view")
 ]

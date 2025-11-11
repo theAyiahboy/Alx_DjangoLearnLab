3c048713c0affdf5eee6e993d342941e5a2da1cd
Giovanni <giovanniayiahmensah@gmail.com>
2025-11-11 21:18:07 +0000

    fix advanced_features_and_security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     # Add the custom fields to the admin interface
@@ -15,3 +14,6 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('username',)
+
+# Explicitly register CustomUser
+admin.site.register(CustomUser, CustomUserAdmin)

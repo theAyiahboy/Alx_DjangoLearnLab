e448bf9841e140bee89de5cb31e564987ce0b8d8
Giovanni <giovanniayiahmensah@gmail.com>
2025-11-21 22:46:52 +0000

    task o1 of api_project

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,3 +1,7 @@
+# api/admin.py
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author')
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# api/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(generics.ListAPIView):
+    """
+    GET /api/books/  -> returns list of books
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer

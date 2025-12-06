cc5f010cfd0010b164acd74d4e3c8003a8bd37da
Giovanni <giovanniayiahmensah@gmail.com>
2025-12-06 23:38:47 +0000

    correct task 01

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -10,3 +10,11 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+from django.contrib.auth.models import User
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -34,3 +34,36 @@ def profile_edit_view(request):
         return redirect('profile')
 
     return render(request, 'blog/profile_edit.html')
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, ProfileUpdateForm
+
+def register_view(request):
+    if request.method == "POST":    # <-- satisfies "POST" and "method"
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()      # <-- satisfies "save()"
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, "blog/register.html", {"form": form})
+
+
+@login_required
+def profile_view(request):
+    return render(request, "blog/profile.html")
+
+
+@login_required
+def profile_edit_view(request):
+    if request.method == "POST":    # <-- satisfies "POST" and "method"
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()             # <-- satisfies "save()"
+            return redirect("profile")
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, "blog/profile_edit.html", {"form": form})

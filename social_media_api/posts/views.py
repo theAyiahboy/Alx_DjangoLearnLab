5678c11c6dc39149d3bd673dce62184e43e0c51c
Giovanni <giovanniayiahmensah@gmail.com>
2025-12-12 19:02:20 +0000

    add task 03

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -83,6 +83,91 @@ class FeedView(generics.ListAPIView):
 # ---------------------------
 # Like a Post
 # ---------------------------
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets, permissions, filters, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Post, Comment, Like
+from .serializers import PostSerializer, CommentSerializer
+from accounts.models import User
+from notifications.models import Notification
+
+# ---------------------------
+# Custom permission
+# ---------------------------
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission: Only the author can edit/delete.
+    Others can read.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+# ---------------------------
+# Post ViewSet
+# ---------------------------
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+# ---------------------------
+# Comment ViewSet
+# ---------------------------
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+# ---------------------------
+# Signal: Notify post author on comment
+# ---------------------------
+@receiver(post_save, sender=Comment)
+def create_comment_notification(sender, instance, created, **kwargs):
+    if created and instance.post.author != instance.author:
+        Notification.objects.create(
+            recipient=instance.post.author,
+            actor=instance.author,
+            verb="commented on your post",
+            target=instance.post
+        )
+
+
+# ---------------------------
+# Feed View
+# ---------------------------
+class FeedView(generics.ListAPIView):
+    """
+    Shows posts from users the current user follows.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+
+# ---------------------------
+# Like a Post
+# ---------------------------
 class LikePostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
